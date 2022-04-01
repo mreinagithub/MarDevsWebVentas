4b5c46f7340a7e315b255677d0723b3e8bfb7c03
Martin Reina <MartinReina84@hotmail.com>
2022-03-31 21:05:38 -0300

    primer commit

---
+++ MarDevsWeb.Venta/wwwroot/Util.js
@@ -0,0 +1,5 @@
+﻿function setFooterText() {
+    document.getElementsByClassName("app-footer")[0].removeAttribute("hidden")
+    document.getElementById("footerId").innerText = "MarDevs® 2021-2022 Todos los derechos reservados. Para consultas y comentarios enviar un e-mail a: infomardevs@gmail.com";    
+}
+
