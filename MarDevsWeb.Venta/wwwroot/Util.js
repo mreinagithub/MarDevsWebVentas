afce282df2a9be28cbac7466ac8a478017529abe
Martin <martinreina84@hotmail.com>
2022-11-20 20:43:24 -0300

    nueva publicacion

--- MarDevsWeb.Venta/wwwroot/Util.js
+++ MarDevsWeb.Venta/wwwroot/Util.js
@@ -1,5 +1,12 @@
 ﻿function setFooterText() {
     document.getElementsByClassName("app-footer")[0].removeAttribute("hidden")
-    document.getElementById("footerId").innerText = "MarDevs® 2021-2022 Todos los derechos reservados. Para consultas y comentarios enviar un e-mail a: infomardevs@gmail.com";    
+    document.getElementById("footerId").innerText = "MarDevs® 2022 Todos los derechos reservados. Para consultas y comentarios enviar un e-mail a: infomardevs@gmail.com";    
 }
 
+function focusElement(id) {
+
+    setTimeout(() => {
+        const element = document.getElementById(id);
+        element.focus()
+    }, 200);
+}
